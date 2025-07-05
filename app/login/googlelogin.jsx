fbd6a8444f93c5fc3d3f3417a1bf1079e21a12d2
Ravi <raviranjanpkr7978@gmail.com>
2025-07-05 19:17:55 +0530

    work on login feature

---
+++ app/login/googlelogin.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { signIn } from "next-auth/react"
+
+export default async function GooglelogIn() {
+  return (
+      <Button onClick={() => signIn("google")} variant="outline" className="w-full p-3">
+        <div className="flex items-center justify-center gap-2 p-3">
+          <Image
+            src="/icons8-google-48.png"
+            alt="Google"
+            width={20}
+            height={20}
+          />
+          <span>Continue with Google</span>
+        </div>
+      </Button>
+  );
+}
